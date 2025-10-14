d6295dbedcb3d2b0d93c20111cd7f273b0814d43
Salvatore <salvatore_scopelliti1993@hotmail.com>
2025-10-14 22:37:05 +1100

    Tidy up

--- capstone2_t3/Assets/sal/CatLike.cs
+++ capstone2_t3/Assets/sal/CatLike.cs
@@ -36,38 +36,3 @@ public class CatLike : ScriptableObject
         return false;
     }
 }
-
-// using UnityEngine;
-// using UnityEngine.UI;
-
-// [CreateAssetMenu(fileName = "New Like", menuName = "Cat/Like")]
-// public class CatLike : ScriptableObject
-// {
-//     public string likeTag;
-//     public Sprite catImage;
-
-//     public bool IsLikeSatisfied(GameObject cat)
-//     {
-//         if (cat == null)
-//         {
-//             Debug.LogWarning("No cat reference provided!");
-//             return false;
-//         }
-
-//         GameObject[] foodObjects = GameObject.FindGameObjectsWithTag(likeTag);
-
-//         foreach (GameObject food in foodObjects)
-//         {
-//             float distance = Vector3.Distance(cat.transform.position, food.transform.position);
-
-//             if (distance <= 5f)
-//             {
-//                 Debug.Log("Cat Like Satisfied : "+ distance.ToString());
-//                 return true;
-//             }
-//         }
-
-//         Debug.Log("No food nearby.");
-//         return false;
-//     }
-// }
