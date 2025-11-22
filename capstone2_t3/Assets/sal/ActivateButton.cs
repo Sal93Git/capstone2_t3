1e6c4ba1e5c4097550ee12ea249701ee75b45932
Salvatore <salvatore_scopelliti1993@hotmail.com>
2025-11-22 11:35:56 +1100

    Level 3 progress commit
    Level 3 progress commit

---
+++ capstone2_t3/Assets/sal/ActivateButton.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class ActivateButton : MonoBehaviour
+{
+    public float detectionRadius = 2f;
+    public bool buttonActive = false;
+
+    private void Update()
+    {
+        buttonActive = CatNearby();
+    }
+
+    bool CatNearby()
+    {
+        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj == this.gameObject) continue;
+            if (!obj.tag.ToLower().Contains("cat")) continue;
+
+            float distance = Vector2.Distance(transform.position, obj.transform.position);
+
+            if (distance <= detectionRadius)
+            {
+                // cat found in range
+                return true; 
+            }
+        }
+
+        // no cats in range
+        return false; 
+    }
+}
